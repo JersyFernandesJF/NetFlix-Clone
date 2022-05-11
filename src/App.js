4a0853015923300de71bf4bade0448bd9d46024b
JersyFernandesJF <jersysilvafernandes@gmail.com>
2022-05-11 15:37:46 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import Tmdb from './Tmdb'
+import MovieRow from './components/MovieRow.js'
+import './App.css'
+import FeatureMovie from './components/FeatureMovie.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default () => {
+
+  const [ movieList, setMovieList ] = useState([])
+  const [ featureData, setFeatureData ] = useState(null)
+
+   useEffect(()=>{
+      const loadAll = async () => {
+        
+        let list = await Tmdb.getHomeList()
+        setMovieList(list)
+
+        // Pegando o Feature
+        let originals = list.filter(i => i.slug ==='originals')
+        let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1 ))
+        let chosen = originals[0].items.results[randomChosen]
+        let chosenInfo = await Tmdb.getMovieInfo( chosen.id, 'tv')
+        
+        setFeatureData(chosenInfo)
+
+      }
+     loadAll()
+   }, [])
+
+
+  return(
+    <div className="page">
+
+      {featureData && <FeatureMovie item ={featureData}/>}
+
+      <section className="lists">
+         {movieList.map((item, key)=>(
+         <MovieRow key={key} title={item.title} items= {item.items} />
+        ))}
+      </section>
     </div>
-  );
+  )
 }
-
-export default App;
